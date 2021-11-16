be10f995070e776b86d32f6c1b00101a6d87919b
Muhammad Daffa Daniswara <70484139+mdaffadaniswara@users.noreply.github.com>
2021-11-16 14:55:11 +0700

    Update Adder.vhd
    Menambah x_init & y_init

--- Adder.vhd
+++ Adder.vhd
@@ -6,27 +6,27 @@ use IEEE.std_logic_unsigned.all;
 entity Adder is
 	port( reset		: IN std_logic; 
 		  comparison: IN std_logic_vector( 1 downto 0 ); 
-		  x, y		: IN std_logic_vector( 7 downto 0 ); 
+		  x_init, x, y_init, y	: IN std_logic_vector( 7 downto 0 ); 
 		  XAddX, YAddY: OUT std_logic_vector( 7 downto 0 ) 
 		);
 end Adder;
  
 architecture Adder_arc of Adder is 
 begin 
-	-- proses untuk menambahkan nilai yang lebih kecil
+	-- proses untuk menambahkan nilai yang lebih kecil dengan nilai awal
 	process( reset, comparison, x, y ) 
 	begin 
 		
-		if( reset = '1' or comparison = "00" ) then 	-- kondisi reset 
+		if( reset = '1' or comparison = "00" ) then		-- kondisi reset 
 			XAddX <= "00000000"; 
 			YAddY <= "00000000"; 
-		elsif( comparison = "10" ) then 			-- x > y
+		elsif( comparison = "10" ) then 				-- x > y
 			XAddX <= x; 		
-			YAddY <= ( y + y ); 						-- Y <= y + y
-		elsif( comparison = "01" ) then 			-- y > x
-			XAddX <= ( x + x );  					-- X <= x + x
+			YAddY <= ( y_init + y ); 				-- Y <= y + y_init
+		elsif( comparison = "01" ) then 				-- y > x
+			XAddX <= ( x_init + x );  				-- X <= x + x_init
 			YAddY <= y; 							
-		else												-- x = y
+		else								-- x = y
 			XAddX <= x; 							
 			YAddY <= y; 
 		end if; 
