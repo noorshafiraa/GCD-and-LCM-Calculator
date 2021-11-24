0743b48e878f721de026ccd451865618a208002b
HakimTralalaTrilili <77867593+HakimTralalaTrilili@users.noreply.github.com>
2021-11-24 21:46:43 +0700

    Add files via upload

---
+++ mult100.vhd
@@ -0,0 +1,30 @@
+-- Quartus Prime VHDL Template
+-- Unsigned Multiply
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity mult100 is
+
+	generic
+	(
+		DATA_WIDTH : natural := 8
+	);
+
+	port 
+	(
+		dig_ratusan	   : in unsigned (7 downto 0);
+		result  : out unsigned (7 downto 0)
+	);
+
+end entity;
+
+architecture rtl of mult100 is
+signal full_product	: unsigned (15 downto 0);
+begin
+
+	full_product <= dig_ratusan * "01100100";
+	result <= full_product (7 downto 0);
+
+end rtl;
