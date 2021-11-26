ae5621ca4af44506ae7d7cb04551df1f6ec86921
HakimTralalaTrilili <77867593+HakimTralalaTrilili@users.noreply.github.com>
2021-11-26 11:04:45 +0700

    Add files via upload

--- ASCII_to_binary.vhd
+++ ASCII_to_binary.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity 	ASCII_to_binary is 
 	port(
 		ASCII_in	: in std_logic_vector(7 downto 0);
-		binary_out	: out std_logic_vector(3 downto 0)
+		binary_out	: out std_logic_vector(7 downto 0)
 		);
 end entity;
 
@@ -14,25 +14,25 @@ begin
 process(ASCII_in)
 begin
 	if ASCII_in = "00110000" then --0
-		binary_out <= "0000"; 
+		binary_out <= "00000000"; 
 	elsif ASCII_in = "00110001" then --1
-		binary_out <= "0001";
+		binary_out <= "00000001";
 	elsif ASCII_in = "00110010" then --2
-		binary_out <= "0010";
+		binary_out <= "00000010";
 	elsif ASCII_in = "00110011" then --3
-		binary_out <= "0011";
+		binary_out <= "00000011";
 	elsif ASCII_in = "00110100" then --4
-		binary_out <= "0100";
+		binary_out <= "00000100";
 	elsif ASCII_in = "00110101" then --5
-		binary_out <= "0101";
+		binary_out <= "00000101";
 	elsif ASCII_in = "00110110" then --6
-		binary_out <= "0110";
+		binary_out <= "00000110";
 	elsif ASCII_in = "00110111" then --7
-		binary_out <= "0111";
+		binary_out <= "00000111";
 	elsif ASCII_in = "00111000" then --8
-		binary_out <= "1000";
+		binary_out <= "00001000";
 	elsif ASCII_in = "00111001" then --9
-		binary_out <= "1001";
+		binary_out <= "00001001";
 	end if;
 
 end process;
